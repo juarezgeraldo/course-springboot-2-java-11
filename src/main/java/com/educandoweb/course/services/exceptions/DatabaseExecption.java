8ff1922a41892e8eecf47080af3939917cc22cc9
Juarez Junior <juarezgeraldosilva@gmail.com>
2020-03-06 14:48:48 -0300

    Exception handling - delete

---
+++ src/main/java/com/educandoweb/course/services/exceptions/DatabaseExecption.java
@@ -0,0 +1,9 @@
+package com.educandoweb.course.services.exceptions;
+
+public class DatabaseExecption extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseExecption(String msg) {
+		super(msg);
+	}
+}
